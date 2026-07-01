315d0844c2b8149855bf2d9021cfffcb66caa555
Mick Jasker <mick@codecapi.nl>
2026-07-01 10:09:35 +0200

    fix(hero): adjust padding and height properties in hero section for improved layout

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -36,7 +36,7 @@ export async function Hero({ locale }: { locale: Locale }) {
   return (
     <div
       data-testid="hero-section"
-      className="grid grid-cols-[24px_1fr_24px] md:grid-cols-[24px_6fr_6fr_6fr_24px] lg:grid-cols-[2fr_3fr_4fr_4fr_2fr] xl:grid-cols-[2fr_3fr_1fr_8fr_2fr] h-[calc(100svh-5.5rem)] md:max-h-225 gap-4 pb-20 md:pb-0 items-end md:items-center bg-brand-forest text-olive-50"
+      className="grid grid-cols-[24px_1fr_24px] md:grid-cols-[24px_6fr_6fr_6fr_24px] lg:grid-cols-[2fr_3fr_4fr_4fr_2fr] xl:grid-cols-[2fr_3fr_1fr_8fr_2fr] min-h-[calc(100svh-5.5rem)] md:max-h-225 gap-4 items-end md:items-center bg-brand-forest text-olive-50"
     >
       <div className="col-span-full md:col-start-3 md:col-end-6 bg-cream-50 w-full h-full row-start-1 row-end-2 relative">
         {heroImageUrl ? (
@@ -63,7 +63,7 @@ export async function Hero({ locale }: { locale: Locale }) {
         )}
         <div className="absolute h-150 md:h-full bottom-0 md:top-0 left-0 w-full md:w-125 bg-linear-to-t md:bg-linear-to-r from-brand-forest to-brand-forest/0"></div>
       </div>
-      <div className="col-start-2 col-end-3 md:col-end-4 row-start-1 row-end-2 relative flex flex-col gap-6 text-sage">
+      <div className="col-start-2 col-end-3 md:col-end-4 row-start-1 row-end-2 relative flex flex-col gap-6 py-20 text-sage">
         <h1 className="contents">
           <Logo className="w-full h-auto" />
         </h1>
